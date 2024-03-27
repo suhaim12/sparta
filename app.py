dcdde21aa74313e407bd55c12fdc0eb0dbc52e3d
Your Name <you@example.com>
2024-03-28 03:38:38 +0700

    First Commit

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+
+from http import client
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
+import requests
+from bs4 import BeautifulSoup
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -17,26 +21,45 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route("/mars", methods=["POST"])
-def web_mars_post():
-    # sample_receive = request.form['sample_give']
-    name_receive = request.form['name_give']
-    address_receive = request.form['address_give']
-    size_receive = request.form['size_give']
+@app.route("/movie", methods=["POST"])
+@app.route("/movie", methods=["POST"])
+def movie_post():
+    url_receive = request.form['url_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    og_image = soup.select_one('meta[property="og:image"]')
+    og_title = soup.select_one('meta[property="og:title"]')
+    og_description = soup.select_one('meta[property="og:description"]')
+
+    image = og_image['content']
+    title = og_title['content']
+    desc = og_description['content']
+
     doc = {
-        'name': name_receive,
-        'address': address_receive,
-        'size': size_receive,
+        'image':image,
+        'title':title,
+        'description':desc,
+        'star':star_receive,
+        'comment':comment_receive
     }
-    db.orders.insert_one(doc)
-    return jsonify({'msg': 'complete!'})
 
-@app.route("/mars", methods=["GET"])
-def web_mars_get():
-    orders_list = list(db.orders.find({}, {'_id': False}))
-    return jsonify({'orders': orders_list})
+    db.movies.insert_one(doc)
+
+    return jsonify({'msg':'POST request!'})
+
+@app.route("/movie", methods=["GET"])
+def movie_get():
+    movie_list = list(db.movies.find({}, {'_id' :False}))
+    return jsonify({'movies': movie_list})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
