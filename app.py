4d2b1df59ce19fb45eea15f4c09dc1096805289e
Your Name <you@example.com>
2024-03-28 04:06:14 +0700

    First Commit

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-
-from http import client
-from flask import Flask, render_template, request, jsonify
-from pymongo import MongoClient
-import requests
-from bs4 import BeautifulSoup
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -21,20 +20,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+   return render_template('index.html')
 
 @app.route("/movie", methods=["POST"])
-@app.route("/movie", methods=["POST"])
 def movie_post():
+    # sample_receive = request.form['sample_give']
     url_receive = request.form['url_give']
     star_receive = request.form['star_give']
     comment_receive = request.form['comment_give']
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get(url_receive, headers=headers)
 
     soup = BeautifulSoup(data.text, 'html.parser')
+
+    # From here on, we will write the code for extracting data from meta tags
 
     og_image = soup.select_one('meta[property="og:image"]')
     og_title = soup.select_one('meta[property="og:title"]')
@@ -42,14 +42,14 @@ def movie_post():
 
     image = og_image['content']
     title = og_title['content']
-    desc = og_description['content']
-
+    description = og_description['content']
+    
     doc = {
-        'image':image,
-        'title':title,
-        'description':desc,
-        'star':star_receive,
-        'comment':comment_receive
+        'image': image,
+        'title': title,
+        'description': description,
+        'star': star_receive,
+        'comment': comment_receive,
     }
 
     db.movies.insert_one(doc)
@@ -58,8 +58,8 @@ def movie_post():
 
 @app.route("/movie", methods=["GET"])
 def movie_get():
-    movie_list = list(db.movies.find({}, {'_id' :False}))
+    movie_list = list(db.movies.find({}, {'_id': False}))
     return jsonify({'movies': movie_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
