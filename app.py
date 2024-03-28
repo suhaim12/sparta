1f63c530ff0147aae23ab12f5bd1e4ae8f778f20
Your Name <you@example.com>
2024-03-28 12:07:53 +0700

    freeze dotenv and other requirements

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+import requests
+from bs4 import BeautifulSoup
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
@@ -12,31 +14,50 @@ DB_NAME =  os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI)
 db = client[DB_NAME]
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
    return render_template('index.html')
 
-@app.route("/mars", methods=["POST"])
-def web_mars_post():
+@app.route("/movie", methods=["POST"])
+def movie_post():
     # sample_receive = request.form['sample_give']
-    name_receive = request.form['name_give']
-    address_receive = request.form['address_give']
-    size_receive = request.form['size_give']
-    doc = {
-        'name': name_receive,
-        'address': address_receive,
-        'size': size_receive,
-    }
-    db.orders.insert_one(doc)
-    return jsonify({'msg': 'complete!'})
+    url_receive = request.form['url_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
 
-@app.route("/mars", methods=["GET"])
-def web_mars_get():
-    orders_list = list(db.orders.find({}, {'_id': False}))
-    return jsonify({'orders': orders_list})
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    # From here on, we will write the code for extracting data from meta tags
+
+    og_image = soup.select_one('meta[property="og:image"]')
+    og_title = soup.select_one('meta[property="og:title"]')
+    og_description = soup.select_one('meta[property="og:description"]')
+
+    image = og_image['content']
+    title = og_title['content']
+    description = og_description['content']
+    
+    doc = {
+        'image': image,
+        'title': title,
+        'description': description,
+        'star': star_receive,
+        'comment': comment_receive,
+    }
+
+    db.movies.insert_one(doc)
+
+    return jsonify({'msg':'POST request!'})
+
+@app.route("/movie", methods=["GET"])
+def movie_get():
+    movie_list = list(db.movies.find({}, {'_id': False}))
+    return jsonify({'movies': movie_list})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
